de2f1bc4fdbcece92a2eb63ead4a09c09395cab8
James Corbett <corbett8@llnl.gov>
2022-05-04 17:09:20 +0000

    dws: group rabbits associated with same allocation
    Problem: DWS expects that an entry in the 'spec' field of a
    Servers resource represents one DW directive. However, for
    simplicity, the code had been splitting DW directives into
    multiple 'spec' entries.
    
    Solution: group the 'spec' entries by their DW directive.

--- src/python/flux_k8s/directivebreakdown.py
+++ src/python/flux_k8s/directivebreakdown.py
@@ -17,6 +17,13 @@ LUSTRE_TYPES = ("ost", "mdt", "mgt")
 def build_allocation_sets(allocation_sets, local_allocations, nodes_per_nnf):
     ret = []
     for allocation in allocation_sets:
+        alloc_entry = {
+            "allocationSize": 0,
+            "label": allocation["label"],
+            "storage": [],
+        }
+        max_alloc_size = 0
+        # build the storage field of alloc_entry
         for nnf_name in local_allocations:
             if allocation["label"] in PER_COMPUTE_TYPES:
                 alloc_size = int(
@@ -29,20 +36,17 @@ def build_allocation_sets(allocation_sets, local_allocations, nodes_per_nnf):
                         "Expected an allocation size of at least "
                         f"{allocation['minimumCapacity']}, got {alloc_size}"
                     )
-                ret.append(
-                    {
-                        "allocationSize": alloc_size,
-                        "label": allocation["label"],
-                        "storage": [
-                            {
-                                "allocationCount": nodes_per_nnf[nnf_name],
-                                "name": nnf_name,
-                            }
-                        ],
-                    }
+                if max_alloc_size == 0:
+                    max_alloc_size = alloc_size
+                else:
+                    max_alloc_size = min(max_alloc_size, alloc_size)
+                alloc_entry["storage"].append(
+                    {"allocationCount": nodes_per_nnf[nnf_name], "name": nnf_name}
                 )
             else:
                 raise ValueError(f"{allocation['label']} not currently supported")
+        alloc_entry["allocationSize"] = max_alloc_size
+        ret.append(alloc_entry)
     return ret
 
 
