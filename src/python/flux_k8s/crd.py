8b471b5081961aea00b101d55d88803ac7682e39
James Corbett <corbett8@llnl.gov>
2023-05-30 11:37:16 -0700

    dws: update CRD versions to v1alpha2
    Problem: HPE has made some changes to their CRDs and
    are putting the changes under the v1alpha2 version.
    For instance, string jobIDs are only supported in
    v1alpha2.
    
    Upgrade the version used by the flux-dws software
    to v1alpha2.

--- src/python/flux_k8s/crd.py
+++ src/python/flux_k8s/crd.py
@@ -4,32 +4,35 @@ CRD = namedtuple("CRD", ["group", "version", "namespace", "plural"])
 
 WORKFLOW_CRD = CRD(
     group="dws.cray.hpe.com",
-    version="v1alpha1",
+    version="v1alpha2",
     namespace="default",
     plural="workflows",
 )
 
 RABBIT_CRD = CRD(
     group="dws.cray.hpe.com",
-    version="v1alpha1",
+    version="v1alpha2",
     namespace="default",
     plural="storages",
 )
 
 DIRECTIVEBREAKDOWN_CRD = CRD(
     group="dws.cray.hpe.com",
-    version="v1alpha1",
+    version="v1alpha2",
     namespace="default",
     plural="directivebreakdowns",
 )
 
 COMPUTE_CRD = CRD(
     group="dws.cray.hpe.com",
-    version="v1alpha1",
+    version="v1alpha2",
     namespace="default",
     plural="computes",
 )
 
 SERVER_CRD = CRD(
-    group="dws.cray.hpe.com", version="v1alpha1", namespace="default", plural="servers",
+    group="dws.cray.hpe.com",
+    version="v1alpha2",
+    namespace="default",
+    plural="servers",
 )
