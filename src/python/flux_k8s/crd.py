b50c65625040a7e3e7d5d641cf0828881ee2ce94
James Corbett <corbett8@llnl.gov>
2023-09-25 16:51:00 -0700

    dws: fix group of k8s crds
    Problem: HPE updates the group of various kubernetes objects from
    'dws.cray.hpe.com' to 'dataworkflowservices.github.io'. This breaks
    code that is looking for resources under the former group.
    
    Update the group to the new name.

--- src/python/flux_k8s/crd.py
+++ src/python/flux_k8s/crd.py
@@ -3,35 +3,35 @@ from collections import namedtuple
 CRD = namedtuple("CRD", ["group", "version", "namespace", "plural"])
 
 WORKFLOW_CRD = CRD(
-    group="dws.cray.hpe.com",
+    group="dataworkflowservices.github.io",
     version="v1alpha2",
     namespace="default",
     plural="workflows",
 )
 
 RABBIT_CRD = CRD(
-    group="dws.cray.hpe.com",
+    group="dataworkflowservices.github.io",
     version="v1alpha2",
     namespace="default",
     plural="storages",
 )
 
 DIRECTIVEBREAKDOWN_CRD = CRD(
-    group="dws.cray.hpe.com",
+    group="dataworkflowservices.github.io",
     version="v1alpha2",
     namespace="default",
     plural="directivebreakdowns",
 )
 
 COMPUTE_CRD = CRD(
-    group="dws.cray.hpe.com",
+    group="dataworkflowservices.github.io",
     version="v1alpha2",
     namespace="default",
     plural="computes",
 )
 
 SERVER_CRD = CRD(
-    group="dws.cray.hpe.com",
+    group="dataworkflowservices.github.io",
     version="v1alpha2",
     namespace="default",
     plural="servers",
--- t/sharness.d/03-kube.sh
+++ t/sharness.d/03-kube.sh
@@ -5,7 +5,7 @@ export KUBECONFIG=${REAL_HOME}/.kube/config
 
 #  Set DWS_K8S or NO_DWS_K8S prereq
 if type "kubectl" > /dev/null \
-   && kubectl get crd 2> /dev/null | grep -q dws.cray.hpe.com; then
+   && kubectl get crd 2> /dev/null | grep -q dataworkflowservices.github.io; then
     test_set_prereq DWS_K8S
 else
     test_set_prereq NO_DWS_K8S
