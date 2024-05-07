432dedbc7d4d995f3d4d92dd0369ff5402ef3dc7
James Corbett <corbett8@llnl.gov>
2024-05-07 14:50:18 -0700

    dws2jgf: drop rabbit vertices from graph
    Problem: in the current way flux-dws2jgf creates resource graphs,
    every rack vertex has a single rabbit child vertex, which in turn
    has numerous ssd children. This makes it difficult for Fluxion to
    schedule when multiple nodes are needed on a single rack.
    
    Remove the 'rabbit' vertices and make the ssd vertices the direct
    children of 'rack'.

--- src/cmd/flux-dws2jgf.py
+++ src/cmd/flux-dws2jgf.py
@@ -105,31 +105,10 @@ class Coral2Graph(FluxionResourceGraphV1):
                 to_gibibytes(nnf["capacity"] // self._chunks_per_nnf),
                 {},
                 f"{parent.path}/{res_name}",
+                1,  # status=1 marks the ssds as 'down' initially
             )
             edg = ElCapResourceRelationshipV1(parent.get_id(), vtx.get_id())
             self._add_and_tick_uniq_id(vtx, edg)
-
-    def _encode_rabbit(self, parent, nnf):
-        res_type = "rabbit"
-        res_name = f"{res_type}-{nnf['metadata']['name']}"
-        vtx = ElCapResourcePoolV1(
-            self._uniqId,
-            res_type,
-            res_type,
-            res_name,
-            self._rackids,
-            self._uniqId,
-            -1,
-            True,
-            "",
-            1,
-            {},
-            f"{parent.path}/{res_name}",
-            1,  # status=1 marks the rabbits as 'down' initially
-        )
-        edg = ElCapResourceRelationshipV1(parent.get_id(), vtx.get_id())
-        self._add_and_tick_uniq_id(vtx, edg)
-        self._encode_ssds(vtx, nnf["status"])
 
     def _encode_rack(self, parent, nnf):
         res_type = "rack"
@@ -150,7 +129,7 @@ class Coral2Graph(FluxionResourceGraphV1):
         )
         edg = ElCapResourceRelationshipV1(parent.get_id(), vtx.get_id())
         self._add_and_tick_uniq_id(vtx, edg)
-        self._encode_rabbit(vtx, nnf)
+        self._encode_ssds(vtx, nnf["status"])
         for node in nnf["status"]["access"].get("computes", []):
             try:
                 index = self._r_hostlist.index(node["name"])[0]
