2f2ece57f98d3744e6da02c258f0dad70a20bbfb
James Corbett <corbett8@llnl.gov>
2024-10-31 16:12:56 -0700

    flux_k8s: reduce string literal duplication
    Problem: the flux_k8s.crd module has a bunch of duplicated string
    literals.
    
    Condense them into constants.

--- src/python/flux_k8s/crd.py
+++ src/python/flux_k8s/crd.py
@@ -2,37 +2,41 @@ from collections import namedtuple
 
 CRD = namedtuple("CRD", ["group", "version", "namespace", "plural"])
 
+DWS_GROUP = "dataworkflowservices.github.io"
+DWS_API_VERSION = "v1alpha2"
+DEFAULT_NAMESPACE = "default"
+
 WORKFLOW_CRD = CRD(
-    group="dataworkflowservices.github.io",
-    version="v1alpha2",
-    namespace="default",
+    group=DWS_GROUP,
+    version=DWS_API_VERSION,
+    namespace=DEFAULT_NAMESPACE,
     plural="workflows",
 )
 
 RABBIT_CRD = CRD(
-    group="dataworkflowservices.github.io",
-    version="v1alpha2",
-    namespace="default",
+    group=DWS_GROUP,
+    version=DWS_API_VERSION,
+    namespace=DEFAULT_NAMESPACE,
     plural="storages",
 )
 
 DIRECTIVEBREAKDOWN_CRD = CRD(
-    group="dataworkflowservices.github.io",
-    version="v1alpha2",
-    namespace="default",
+    group=DWS_GROUP,
+    version=DWS_API_VERSION,
+    namespace=DEFAULT_NAMESPACE,
     plural="directivebreakdowns",
 )
 
 COMPUTE_CRD = CRD(
-    group="dataworkflowservices.github.io",
-    version="v1alpha2",
-    namespace="default",
+    group=DWS_GROUP,
+    version=DWS_API_VERSION,
+    namespace=DEFAULT_NAMESPACE,
     plural="computes",
 )
 
 SERVER_CRD = CRD(
-    group="dataworkflowservices.github.io",
-    version="v1alpha2",
-    namespace="default",
+    group=DWS_GROUP,
+    version=DWS_API_VERSION,
+    namespace=DEFAULT_NAMESPACE,
     plural="servers",
 )
