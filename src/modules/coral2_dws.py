1f0133c438f5ff8a0a0260390639638bcf532a04
James Corbett <corbett8@llnl.gov>
2025-02-26 13:29:29 -0800

    `coral2_dws`: refactor workflow code into module
    Problem: the `coral2_dws` script is long and complicated, and should
    be broken up into smaller pieces.
    
    Break out some workflow classes and functions into a separate module.

--- src/modules/coral2_dws.py
+++ src/modules/coral2_dws.py
@@ -30,78 +30,22 @@ from flux_k8s import crd
 from flux_k8s.watch import Watchers, Watch
 from flux_k8s import directivebreakdown
 from flux_k8s import cleanup
+from flux_k8s.workflow import (
+    TransientConditionInfo,
+    WorkflowInfo,
+    WORKFLOW_NAME_PREFIX,
+    WORKFLOW_NAME_FORMAT,
+    save_workflow_to_kvs,
+)
 
 
 _HOSTNAMES_TO_RABBITS = {}  # maps compute hostnames to rabbit names
 _RABBITS_TO_HOSTLISTS = {}  # maps rabbits to hostlists
 LOGGER = logging.getLogger(__name__)
 WORKFLOWS_IN_TC = set()  # tc for TransientCondition
-WORKFLOW_NAME_PREFIX = "fluxjob-"
-WORKFLOW_NAME_FORMAT = WORKFLOW_NAME_PREFIX + "{jobid}"
 _MIN_ALLOCATION_SIZE = 4  # minimum rabbit allocation size
 EXCLUDE_PROPERTY = "badrabbit"
-
 _EXITCODE_NORESTART = 3  # exit code indicating to systemd not to restart
-
-
-class WorkflowInfo:
-    """Represents and holds information about a specific workflow object.
-
-    The class offers methods for maintaining a set of instances.
-    """
-
-    save_datamovements = 0
-
-    _WORKFLOWINFO_CACHE = {}  # maps jobids to WorkflowInfo objects
-
-    @classmethod
-    def add(cls, jobid, *args, **kwargs):
-        """Add an entry to the cache of instances."""
-        new_instance = cls(jobid, *args, **kwargs)
-        cls._WORKFLOWINFO_CACHE[new_instance.jobid] = new_instance
-        return new_instance
-
-    @classmethod
-    def get(cls, jobid):
-        """Return an instance with the given jobid."""
-        return cls._WORKFLOWINFO_CACHE.setdefault(jobid, cls(jobid))
-
-    @classmethod
-    def remove(cls, jobid):
-        """Remove an instance with the given jobid."""
-        del cls._WORKFLOWINFO_CACHE[jobid]
-
-    def __init__(self, jobid, name=None, resources=None):
-        self.jobid = jobid
-        if name is None:
-            self.name = WORKFLOW_NAME_FORMAT.format(jobid=jobid)
-        else:
-            self.name = name  # name of the k8s workflow
-        self.resources = resources  # jobspec 'resources' field
-        self.transient_condition = None  # may be a TransientConditionInfo
-        self.toredown = False  # True if workflows has been moved to teardown
-        self.deleted = False  # True if delete request has been sent to k8s
-
-    def move_to_teardown(self, handle, k8s_api, workflow=None):
-        """Move a workflow to the 'Teardown' desiredState."""
-        if workflow is None:
-            workflow = k8s_api.get_namespaced_custom_object(
-                *crd.WORKFLOW_CRD, self.name
-            )
-        datamovements = get_datamovements(k8s_api, self.name, self.save_datamovements)
-        save_workflow_to_kvs(handle, self.jobid, workflow, datamovements)
-        cleanup.teardown_workflow(workflow)
-        self.toredown = True
-
-
-class TransientConditionInfo:
-    """Represents and holds information about a TransientCondition for a workflow."""
-
-    def __init__(self, workflow):
-        self.workflow = workflow  # workflow that hit the Transientcondition
-        self.last_time = time.time()  # time in seconds of last TransientCondition
-        # message associated with last TransientCondition
-        self.last_message = None
 
 
 def log_rpc_response(rpc):
@@ -156,77 +100,6 @@ def save_elapsed_time_to_kvs(handle, jobid, workflow):
     try:
         kvsdir = flux.job.job_kvs(handle, jobid)
         kvsdir[f"rabbit_{state}_timing"] = timing
-        kvsdir.commit()
-    except Exception:
-        LOGGER.exception(
-            "Failed to update KVS for job %s: workflow is %s", jobid, workflow
-        )
-
-
-def get_datamovements(k8s_api, workflow_name, count):
-    """Fetch datamovement resources and optionally dump them to the logs.
-
-    Save every datamovement to the logs if loglevel is INFO or more verbose.
-
-    Return 'count' datamovements.
-    """
-    if LOGGER.isEnabledFor(logging.INFO):
-        limit_arg = {}
-    else:
-        if count <= 0:
-            return []
-        limit_arg = {"limit": count}
-    try:
-        api_response = k8s_api.list_cluster_custom_object(
-            group="nnf.cray.hpe.com",
-            version="v1alpha4",
-            plural="nnfdatamovements",
-            label_selector=(
-                f"dataworkflowservices.github.io/workflow.name={workflow_name},"
-                "dataworkflowservices.github.io/workflow.namespace=default"
-            ),
-            **limit_arg,
-        )
-    except Exception as exc:
-        LOGGER.warning(
-            "Failed to fetch nnfdatamovement crds for workflow '%s': %s",
-            workflow_name,
-            exc,
-        )
-        return []
-    datamovements = []
-    successful_datamovements = []
-    for dm_crd in api_response["items"]:
-        LOGGER.info(
-            "Found nnfdatamovement crd for workflow '%s': %s",
-            workflow_name,
-            json.dumps(dm_crd),
-        )
-        if len(datamovements) < count:
-            if dm_crd["status"]["status"] == "Failed":
-                datamovements.append(dm_crd)
-            else:
-                successful_datamovements.append(dm_crd)
-    if len(datamovements) < count:
-        datamovements.extend(successful_datamovements[: count - len(datamovements)])
-    return datamovements
-
-
-def save_workflow_to_kvs(handle, jobid, workflow, datamovements=None):
-    """Save a workflow to a job's KVS, ignoring errors."""
-    try:
-        timing = workflow["status"]["elapsedTimeLastState"]
-        state = workflow["status"]["state"].lower()
-    except KeyError:
-        timing = None
-        state = None
-    try:
-        kvsdir = flux.job.job_kvs(handle, jobid)
-        kvsdir["rabbit_workflow"] = workflow
-        if timing is not None and state is not None:
-            kvsdir[f"rabbit_{state}_timing"] = timing
-        if datamovements is not None:
-            kvsdir["rabbit_datamovements"] = datamovements
         kvsdir.commit()
     except Exception:
         LOGGER.exception(
